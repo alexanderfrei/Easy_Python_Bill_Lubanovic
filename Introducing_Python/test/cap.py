2415bf39a60a28310593bcb9b8c38cfd518d55e6
alexanderfrei <whutmail@gmail.com>
2017-06-02 13:49:19 +0300

    cls

--- Introducing_Python/test/cap.py
+++
@@ -1,3 +0,0 @@
-def just_do_it(text):
-    return text.title()
-    # return text.capitalize()
--- Introducing_Python/web/bottle_test.py
+++
@@ -1,6 +0,0 @@
-import requests
-resp = requests.get('http://localhost:9999/echo/Pikachu')
-if resp.status_code == 200 and resp.text == 'Say hello to my little friend: Pikachu':
-    print('It worked!')
-else:
-    print('No way (:', resp.text)
